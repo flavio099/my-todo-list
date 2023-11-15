329dcdf4eca7e4453ec8a2618973705065431581
flavio <flavioumberto1@gmail.com>
2023-11-15 11:46:02 +0200

    o casser le js de mytodo list

---
+++ DOM.js
@@ -0,0 +1,39 @@
+/*
+let title = document.querySelector('.title')
+
+let bnt2 = document.querySelector('.btn2')
+bnt1.addEventListener('click', function () { title.innerHTML = 'bienvenu' })
+
+bnt2.addEventListener('click', function () { title.innerHTML = 'aurevoir' })
+
+
+
+let input = document.querySelector('input')
+let title = document.querySelector('.title')
+let bnt2 = document.querySelector('.btn1')
+bnt2.addEventListener('click', function () { title.innerHTML = 'Bienvenue '+ input.value 
+input.value = ""})
+ 
+
+document.body.style.color = 'red'
+document.body.style.border = " 3px dotted  pink "
+document.body.style.alignContent
+
+*/
+let bnt1 = document.querySelector('.btn1')
+let bnt2 = document.querySelector('.btn2')
+let title = document.querySelector('.title')
+let input = document.querySelector('.input')
+let list = document.querySelector('.list')
+let element = document.querySelector('.element')
+let li = document.createElement('li')
+let checkbox = document.createElement('input')
+bnt1.addEventListener('click', function(){ let li = document.createElement('li') 
+li.innerHTML =  input.value
+list.appendChild(li)  
+input.value = ""})
+
+bnt2.addEventListener('click', function(){ let li = document.createElement('li')  
+li.innerHTML =  input.value 
+list.removeChild(li) }) 
+
